b704a6f5cdd4d45ec9229955cb26f8029758febb
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2023-09-22 01:16:00 +0200

    Week 2 Team Activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,25 @@ document.querySelector("#year").textContent = currentDate.getFullYear();
 let oLastModif = new Date(document.lastModified);
 document.querySelector("#modified").textContent = oLastModif;
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("☑️")) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        modeButton.textContent = "❎";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "☑️";
+    }
+});
