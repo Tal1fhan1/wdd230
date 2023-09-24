59625459d2c3aa3c2f4a65be17cda86f1a5578d4
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2023-09-24 07:16:12 +0200

    week 2

--- chamber/scripts/chamber-script.js
+++ chamber/scripts/chamber-script.js
@@ -8,18 +8,3 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
-
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-
-modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("☑️")) {
-        main.style.background = "#000";
-        main.style.color = "#fff";
-        modeButton.textContent = "❎";
-    } else {
-        main.style.background = "#eee";
-        main.style.color = "#000";
-        modeButton.textContent = "☑️";
-    }
-});
