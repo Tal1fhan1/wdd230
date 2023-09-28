6f08c62b4dbc6c95d439cd6a4eabb197ba024252
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2023-09-29 00:21:31 +0200

    Team Activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -26,3 +26,13 @@ modeButton.addEventListener("click", () => {
         modeButton.textContent = "☑️";
     }
 });
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+visitsDisplay.textContent = numVisits;
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
