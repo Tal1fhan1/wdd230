625946a1e688893d11e9bdef8a0061ddc9da7ee5
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2023-10-14 04:08:16 +0200

    eg

--- chamber/scripts/chamber-script.js
+++ chamber/scripts/chamber-script.js
@@ -1,3 +1,95 @@
+const membersURL = "https://tal1fhan1.github.io/wdd230/chamber/data/members.json";
+
+async function getMembers(membersURL) {
+    const response = await fetch(membersURL);
+    const data = await response.json();
+    console.log(data.members[0]);
+    displayMembers(data.members[0]);
+}
+
+getMembers(membersURL);
+
+
+const displayMembers = () => {
+    members.forEach((member) => {
+
+        // let logo1 = document.createElement('img');
+        let section = document.getElementById('company');
+        let name1 = document.createElement('h3');
+        name1.textContent = 'hi there';
+        section.appendChild(name1);
+        console.log(name1)
+        // let address1 = document.createElement('p');
+        // let phone1 = document.createElement('p');
+        // let website1 = document.createElement('p');
+        // let membership1 = document.createElement('p');
+
+        // logo1.setAttribute = ('src', `${member.logo}`);
+
+
+        // address1.textContent = `${member.members[0].company[0].address}`;
+        // phone1.textContent = `${member.members[0].company[0].phone}`;
+        // website1.textContent = `${member.members[0].company[0].website}`;
+        // website1.setAttribute = ('href', `${member.members[0].company[0].website}`);
+        // membership1.textContent = `${member.members[0].company[0].membership}`;
+
+        // section.appendChild(logo1);
+
+        // section.appendChild(address1);
+        // section.appendChild(phone1);
+        // section.appendChild(website1);
+        // section.appendChild(membership1);
+
+
+        // item.textContent = `${week.week}: `
+        // title2.textContent = `${week.links[1].title}`
+        // divide2.textContent = ' | '
+        // title2.setAttribute('href', `${week.links[1].url}`)
+
+        // title3.textContent = `${week.links[2].title}`
+        // divide3.textContent = ' | '
+        // title3.setAttribute('href', `${week.links[2].url}`)
+
+        // item.appendChild(title1);
+        // item.appendChild(divide1)
+        // list.appendChild(item)
+
+        // item.appendChild(title2);
+        // item.appendChild(divide2)
+        // list.appendChild(item)
+
+        // item.appendChild(title3);
+        // item.appendChild(divide3)
+        // list.appendChild(item)
+
+        // if (`${week.week}` == 'Week 3') {
+        //     let title4 = document.createElement('a');
+        //     title4.textContent = `${week.links[3].title}`
+        //     title4.setAttribute('href', `${week.links[3].url}`)
+        //     item.appendChild(title4);
+        //     list.appendChild(item);
+        // }
+        // else if (`${week.week}` == 'Week 4') {
+        //     let title4 = document.createElement('a');
+        //     title4.textContent = `${week.links[3].title}`
+        //     title4.setAttribute('href', `${week.links[3].url}`)
+        //     item.appendChild(title4);
+        //     list.appendChild(item);
+        // }
+        // else if (`${week.week}` == 'Week 5') {
+        //     let title4 = document.createElement('a');
+        //     let divide4 = document.createElement('a');
+        //     title4.textContent = `${week.links[3].title}`
+        //     title4.setAttribute('href', `${week.links[3].url}`)
+        //     item.appendChild(title4);
+        //     list.appendChild(item);
+        // }
+    });
+}
+
+
+
+
 let oLastModif = new Date(document.lastModified);
 document.querySelector("#modified").textContent = oLastModif;
 
