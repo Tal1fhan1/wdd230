037dd0f332e9f52f8ad0a05dddb692c992bdf853
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2023-10-14 11:07:48 +0200

    gfygsd

--- chamber/scripts/chamber-script.js
+++ chamber/scripts/chamber-script.js
@@ -1,45 +1,200 @@
+const url = 'https://www.nchsoftware.com/photoeditor/screenshots/photopadscreen-3.jpg'
 const membersURL = "https://tal1fhan1.github.io/wdd230/chamber/data/members.json";
 
 async function getMembers(membersURL) {
     const response = await fetch(membersURL);
     const data = await response.json();
-    console.log(data.members[0]);
-    displayMembers(data.members[0]);
+    console.log(data.members[0].companies[0].website);
+    displayMembers(data.members);
 }
 
 getMembers(membersURL);
 
 
-const displayMembers = () => {
+const displayMembers = (members) => {
     members.forEach((member) => {
-
-        // let logo1 = document.createElement('img');
-        let section = document.getElementById('company');
+        let section1 = document.getElementById('company');
+        let div1 = document.createElement('div');
+        div1.setAttribute('id', 'company1')
         let name1 = document.createElement('h3');
-        name1.textContent = 'hi there';
-        section.appendChild(name1);
-        console.log(name1)
-        // let address1 = document.createElement('p');
-        // let phone1 = document.createElement('p');
-        // let website1 = document.createElement('p');
-        // let membership1 = document.createElement('p');
+        let logo1 = document.createElement('img');
+        let address1 = document.createElement('p');
+        let phone1 = document.createElement('p');
+        let website1 = document.createElement('span');
+        let membership1 = document.createElement('p');
 
-        // logo1.setAttribute = ('src', `${member.logo}`);
+        let section2 = document.getElementById('company');
+        let div2 = document.createElement('div');
+        div2.setAttribute('id', 'company2');
+        let name2 = document.createElement('h3');
+        let logo2 = document.createElement('img');
+        let address2 = document.createElement('p');
+        let phone2 = document.createElement('p');
+        let website2 = document.createElement('span');
+        let membership2 = document.createElement('p');
+
+        let section3 = document.getElementById('company');
+        let div3 = document.createElement('div');
+        div3.setAttribute('id', 'company3');
+        let name3 = document.createElement('h3');
+        let logo3 = document.createElement('img');
+        let address3 = document.createElement('p');
+        let phone3 = document.createElement('p');
+        let website3 = document.createElement('span');
+        let membership3 = document.createElement('p');
+
+        let section4 = document.getElementById('company');
+        let div4 = document.createElement('div');
+        div4.setAttribute('id', 'company4');
+        let name4 = document.createElement('h3');
+        let logo4 = document.createElement('img');
+        let address4 = document.createElement('p');
+        let phone4 = document.createElement('p');
+        let website4 = document.createElement('span');
+        let membership4 = document.createElement('p');
+
+        let section5 = document.getElementById('company');
+        let div5 = document.createElement('div');
+        div5.setAttribute('id', 'company5');
+        let name5 = document.createElement('h3');
+        let logo5 = document.createElement('img');
+        let address5 = document.createElement('p');
+        let phone5 = document.createElement('p');
+        let website5 = document.createElement('span');
+        let membership5 = document.createElement('p');
+
+        let section6 = document.getElementById('company');
+        let div6 = document.createElement('div');
+        div6.setAttribute('id', 'company6');
+        let name6 = document.createElement('h3');
+        let logo6 = document.createElement('img');
+        let address6 = document.createElement('p');
+        let phone6 = document.createElement('p');
+        let website6 = document.createElement('span');
+        let membership6 = document.createElement('p');
+
+        let section7 = document.getElementById('company');
+        let div7 = document.createElement('div');
+        div7.setAttribute('id', 'company7');
+        let name7 = document.createElement('h3');
+        let logo7 = document.createElement('img');
+        let address7 = document.createElement('p');
+        let phone7 = document.createElement('p');
+        let website7 = document.createElement('span');
+        let membership7 = document.createElement('p');
 
 
-        // address1.textContent = `${member.members[0].company[0].address}`;
-        // phone1.textContent = `${member.members[0].company[0].phone}`;
-        // website1.textContent = `${member.members[0].company[0].website}`;
-        // website1.setAttribute = ('href', `${member.members[0].company[0].website}`);
-        // membership1.textContent = `${member.members[0].company[0].membership}`;
+        name1.textContent = `${member.companies[0].name}`;
+        address1.textContent = `${member.companies[0].address}`;
+        phone1.textContent = `${member.companies[0].phone}`;
+        website1.textContent = `${member.companies[0].website}`;
+        website1.setAttribute = ('href', `${member.companies[0].website}`);
+        membership1.textContent = `Membership: ${member.companies[0].membership}`;
+        logo1.setAttribute = ('src', url);
+        logo1.setAttribute = ('alt', 'what?');
+        div1.appendChild(logo1);
+        div1.appendChild(name1);
+        div1.appendChild(address1);
+        div1.appendChild(phone1);
+        div1.appendChild(website1);
+        div1.appendChild(membership1);
+        section1.appendChild(div1)
 
-        // section.appendChild(logo1);
+        name2.textContent = `${member.companies[1].name}`;
+        address2.textContent = `${member.companies[1].address}`;
+        phone2.textContent = `${member.companies[1].phone}`;
+        website2.textContent = `${member.companies[1].website}`;
+        website2.setAttribute = ('href', `${member.companies[1].website}`);
+        membership2.textContent = `Membership: ${member.companies[1].membership}`;
+        logo2.setAttribute = ('src', url);
+        logo2.setAttribute = ('alt', 'what?');
+        div2.appendChild(logo2);
+        div2.appendChild(name2);
+        div2.appendChild(address2);
+        div2.appendChild(phone2);
+        div2.appendChild(website2);
+        div2.appendChild(membership2);
+        section2.appendChild(div2);
 
-        // section.appendChild(address1);
-        // section.appendChild(phone1);
-        // section.appendChild(website1);
-        // section.appendChild(membership1);
+        name3.textContent = `${member.companies[2].name}`;
+        address3.textContent = `${member.companies[2].address}`;
+        phone3.textContent = `${member.companies[2].phone}`;
+        website3.textContent = `${member.companies[2].website}`;
+        website3.setAttribute = ('href', `${member.companies[2].website}`);
+        membership1.textContent = `Membership: ${member.companies[2].membership}`;
+        logo3.setAttribute = ('src', url);
+        logo3.setAttribute = ('alt', 'what?');
+        div3.appendChild(logo3);
+        div3.appendChild(name3);
+        div3.appendChild(address3);
+        div3.appendChild(phone3);
+        div3.appendChild(website3);
+        div3.appendChild(membership3);
+        section3.appendChild(div3);
 
+        name4.textContent = `${member.companies[3].name}`;
+        address4.textContent = `${member.companies[3].address}`;
+        phone4.textContent = `${member.companies[3].phone}`;
+        website4.textContent = `${member.companies[3].website}`;
+        website4.setAttribute = ('href', `${member.companies[3].website}`);
+        membership4.textContent = `Membership: ${member.companies[3].membership}`;
+        logo4.setAttribute = ('src', url);
+        logo4.setAttribute = ('alt', 'what?');
+        div4.appendChild(logo4);
+        div4.appendChild(name4);
+        div4.appendChild(address4);
+        div4.appendChild(phone4);
+        div4.appendChild(website4);
+        div4.appendChild(membership4);
+        section4.appendChild(div4);
+
+        name5.textContent = `${member.companies[4].name}`;
+        address5.textContent = `${member.companies[4].address}`;
+        phone5.textContent = `${member.companies[4].phone}`;
+        website5.textContent = `${member.companies[4].website}`;
+        website5.setAttribute = ('href', `${member.companies[4].website}`);
+        membership5.textContent = `Membership: ${member.companies[4].membership}`;
+        logo5.setAttribute = ('src', url);
+        logo5.setAttribute = ('alt', 'what?');
+        div5.appendChild(logo5);
+        div5.appendChild(name5);
+        div5.appendChild(address5);
+        div5.appendChild(phone5);
+        div5.appendChild(website5);
+        div5.appendChild(membership5);
+        section5.appendChild(div5);
+
+        name6.textContent = `${member.companies[5].name}`;
+        address6.textContent = `${member.companies[5].address}`;
+        phone6.textContent = `${member.companies[5].phone}`;
+        website6.textContent = `${member.companies[5].website}`;
+        website6.setAttribute = ('href', `${member.companies[5].website}`);
+        membership6.textContent = `Membership: ${member.companies[5].membership}`;
+        logo6.setAttribute = ('src', url);
+        logo6.setAttribute = ('alt', 'what?');
+        div6.appendChild(logo6);
+        div6.appendChild(name6);
+        div6.appendChild(address6);
+        div6.appendChild(phone6);
+        div6.appendChild(website6);
+        div6.appendChild(membership6);
+        section6.appendChild(div6);
+
+        name7.textContent = `${member.companies[6].name}`;
+        address7.textContent = `${member.companies[6].address}`;
+        phone7.textContent = `${member.companies[6].phone}`;
+        website7.textContent = `${member.companies[6].website}`;
+        website7.setAttribute = ('href', `${member.companies[6].website}`);
+        membership7.textContent = `Membership: ${member.companies[6].membership}`;
+        logo7.setAttribute = ('src', url);
+        logo7.setAttribute = ('alt', 'what?');
+        div7.appendChild(logo7);
+        div7.appendChild(name7);
+        div7.appendChild(address7);
+        div7.appendChild(phone7);
+        div7.appendChild(website7);
+        div7.appendChild(membership7);
+        section7.appendChild(div7);
 
         // item.textContent = `${week.week}: `
         // title2.textContent = `${week.links[1].title}`
