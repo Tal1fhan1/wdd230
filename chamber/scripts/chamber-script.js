f7e216d4617c8b62a12f91223b4b164221840ad1
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2023-10-17 01:52:58 +0200

    fixed

--- chamber/scripts/chamber-script.js
+++ chamber/scripts/chamber-script.js
@@ -12,9 +12,9 @@ getMembers(membersURL);
 
 const displayMembers = (members) => {
     members.forEach((member) => {
-        let section1 = document.getElementById('company');
+        let section1 = document.getElementById('company-grid');
         let div1 = document.createElement('div');
-        div1.setAttribute('id', 'company1')
+        div1.setAttribute('id', 'company1-grid')
         let name1 = document.createElement('h3');
         let logo1 = document.createElement('img');
         let address1 = document.createElement('p');
@@ -22,9 +22,9 @@ const displayMembers = (members) => {
         let website1 = document.createElement('a');
         let membership1 = document.createElement('p');
 
-        let section2 = document.getElementById('company');
+        let section2 = document.getElementById('company-grid');
         let div2 = document.createElement('div');
-        div2.setAttribute('id', 'company2');
+        div2.setAttribute('id', 'company2-grid');
         let name2 = document.createElement('h3');
         let logo2 = document.createElement('img');
         let address2 = document.createElement('p');
@@ -32,9 +32,9 @@ const displayMembers = (members) => {
         let website2 = document.createElement('a');
         let membership2 = document.createElement('p');
 
-        let section3 = document.getElementById('company');
+        let section3 = document.getElementById('company-grid');
         let div3 = document.createElement('div');
-        div3.setAttribute('id', 'company3');
+        div3.setAttribute('id', 'company3-grid');
         let name3 = document.createElement('h3');
         let logo3 = document.createElement('img');
         let address3 = document.createElement('p');
@@ -42,9 +42,9 @@ const displayMembers = (members) => {
         let website3 = document.createElement('a');
         let membership3 = document.createElement('p');
 
-        let section4 = document.getElementById('company');
+        let section4 = document.getElementById('company-grid');
         let div4 = document.createElement('div');
-        div4.setAttribute('id', 'company4');
+        div4.setAttribute('id', 'company4-grid');
         let name4 = document.createElement('h3');
         let logo4 = document.createElement('img');
         let address4 = document.createElement('p');
@@ -52,9 +52,9 @@ const displayMembers = (members) => {
         let website4 = document.createElement('a');
         let membership4 = document.createElement('p');
 
-        let section5 = document.getElementById('company');
+        let section5 = document.getElementById('company-grid');
         let div5 = document.createElement('div');
-        div5.setAttribute('id', 'company5');
+        div5.setAttribute('id', 'company5-grid');
         let name5 = document.createElement('h3');
         let logo5 = document.createElement('img');
         let address5 = document.createElement('p');
@@ -62,9 +62,9 @@ const displayMembers = (members) => {
         let website5 = document.createElement('a');
         let membership5 = document.createElement('p');
 
-        let section6 = document.getElementById('company');
+        let section6 = document.getElementById('company-grid');
         let div6 = document.createElement('div');
-        div6.setAttribute('id', 'company6');
+        div6.setAttribute('id', 'company6-grid');
         let name6 = document.createElement('h3');
         let logo6 = document.createElement('img');
         let address6 = document.createElement('p');
@@ -72,9 +72,9 @@ const displayMembers = (members) => {
         let website6 = document.createElement('a');
         let membership6 = document.createElement('p');
 
-        let section7 = document.getElementById('company');
+        let section7 = document.getElementById('company-grid');
         let div7 = document.createElement('div');
-        div7.setAttribute('id', 'company7');
+        div7.setAttribute('id', 'company7-grid');
         let name7 = document.createElement('h3');
         let logo7 = document.createElement('img');
         let address7 = document.createElement('p');
@@ -93,6 +93,7 @@ const displayMembers = (members) => {
         logo1.setAttribute('alt', `${member.companies[0].name} logo`);
         logo1.setAttribute('width', '160px');
         logo1.setAttribute('height', '106px');
+        logo1.setAttribute('loading', 'lazy');
         div1.appendChild(logo1);
         div1.appendChild(name1);
         div1.appendChild(address1);
@@ -111,6 +112,7 @@ const displayMembers = (members) => {
         logo2.setAttribute('alt', `${member.companies[1].name} logo`);
         logo2.setAttribute('width', '203px');
         logo2.setAttribute('height', '114px');
+        logo2.setAttribute('loading', 'lazy');
         div2.appendChild(logo2);
         div2.appendChild(name2);
         div2.appendChild(address2);
@@ -129,6 +131,7 @@ const displayMembers = (members) => {
         logo3.setAttribute('alt', `${member.companies[2].name} logo`);
         logo3.setAttribute('width', '125px');
         logo3.setAttribute('height', '125px');
+        logo3.setAttribute('loading', 'lazy');
         div3.appendChild(logo3);
         div3.appendChild(name3);
         div3.appendChild(address3);
@@ -146,7 +149,8 @@ const displayMembers = (members) => {
         logo4.setAttribute('src', member.companies[3].logo);
         logo4.setAttribute('alt', `${member.companies[3].name} logo`);
         logo4.setAttribute('width', '217px');
-        logo4.setAttribute('height', '66px');;
+        logo4.setAttribute('height', '66px');
+        logo4.setAttribute('loading', 'lazy');
         div4.appendChild(logo4);
         div4.appendChild(name4);
         div4.appendChild(address4);
@@ -165,6 +169,7 @@ const displayMembers = (members) => {
         logo5.setAttribute('alt', `${member.companies[4].name} logo`);
         logo5.setAttribute('width', '200px');
         logo5.setAttribute('height', '60px');
+        logo5.setAttribute('loading', 'lazy');
         div5.appendChild(logo5);
         div5.appendChild(name5);
         div5.appendChild(address5);
@@ -182,7 +187,8 @@ const displayMembers = (members) => {
         logo6.setAttribute('src', member.companies[5].logo);
         logo6.setAttribute('alt', `${member.companies[5].name} logo`);
         logo6.setAttribute('width', '200px');
-        logo6.setAttribute('height', '191px');
+        logo6.setAttribute('height', '50px');
+        logo6.setAttribute('loading', 'lazy');
         div6.appendChild(logo6);
         div6.appendChild(name6);
         div6.appendChild(address6);
@@ -201,6 +207,7 @@ const displayMembers = (members) => {
         logo7.setAttribute('alt', `${member.companies[6].name} logo`);
         logo7.setAttribute('width', '160px');
         logo7.setAttribute('height', '106px');
+        logo7.setAttribute('loading', 'lazy');
         div7.appendChild(logo7);
         div7.appendChild(name7);
         div7.appendChild(address7);
@@ -211,7 +218,36 @@ const displayMembers = (members) => {
     });
 }
 
+const gridbutton = document.querySelector(".grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#company-grid");
 
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.setAttribute('id', 'company-grid')
+    document.querySelector('#company1-list').setAttribute('id', 'company1-grid');
+    document.querySelector('#company2-list').setAttribute('id', 'company2-grid');
+    document.querySelector('#company3-list').setAttribute('id', 'company3-grid');
+    document.querySelector('#company4-list').setAttribute('id', 'company4-grid');
+    document.querySelector('#company5-list').setAttribute('id', 'company5-grid');
+    document.querySelector('#company6-list').setAttribute('id', 'company6-grid');
+    document.querySelector('#company7-list').setAttribute('id', 'company7-grid');
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+    display.setAttribute('id', 'company-list')
+    document.querySelector('#company1-grid').setAttribute('id', 'company1-list');
+    document.querySelector('#company2-grid').setAttribute('id', 'company2-list');
+    document.querySelector('#company3-grid').setAttribute('id', 'company3-list');
+    document.querySelector('#company4-grid').setAttribute('id', 'company4-list');
+    document.querySelector('#company5-grid').setAttribute('id', 'company5-list');
+    document.querySelector('#company6-grid').setAttribute('id', 'company6-list');
+    document.querySelector('#company7-grid').setAttribute('id', 'company7-list');
+}
 
 
 let oLastModif = new Date(document.lastModified);
